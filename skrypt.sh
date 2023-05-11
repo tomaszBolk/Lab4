3d8a08e51ba5019ebbf8dc53c8082293613a9621
Tomasz Bolko <tomaso.bolko@gmail.com>
2023-05-11 21:17:29 +0200

    zmodyfikowano skrypt o mozliwosc wywolania z flaga date

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-DATE=$(date) ; echo “Aktualna data: ${DATE}”;
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-a, --action=ACTION       specify an action to use"
+      echo "-o, --output-dir=DIR      specify a directory to store output in"
+      exit 0
+      ;;
+    --date)
+    shift
+     DATE=$(date) ; echo “Aktualna data: ${DATE}”
+     ;;   
+    *)
+      break
+      ;;
+  esac
+done
+
 
