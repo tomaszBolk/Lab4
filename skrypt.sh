6bbbe5040b3b523b2223048eb987e3027966aee2
Tomasz Bolko <tomaso.bolko@gmail.com>
2023-05-11 21:19:41 +0200

    zmodyfikowano skrypt

--- skrypt.sh
+++ skrypt.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 while test $# -gt 0; do
   case "$1" in
-    -h|--help)
-      echo "options:"
-      echo "-h, --help                show brief help"
-      echo "-a, --action=ACTION       specify an action to use"
-      echo "-o, --output-dir=DIR      specify a directory to store output in"
-      exit 0
-      ;;
     --date)
     shift
      DATE=$(date) ; echo “Aktualna data: ${DATE}”
