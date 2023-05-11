47c35209e6517c407c5f88c9fe76c684d47e4b29
Tomasz Bolko <tomaso.bolko@gmail.com>
2023-05-11 22:07:13 +0200

    zmodyfikowano skrypt o funkcjonalnosc tworzenia logow

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+function createFilesWithDate {
+   me=`basename "$0"`;
+   for i in {1..100}; do
+    echo log${i}.txt $(date) $me > log${i}.txt;
+ done;
+}
 while test $# -gt 0; do
   case "$1" in
     --date)
     shift
      DATE=$(date) ; echo “Aktualna data: ${DATE}”
+     ;;
+     --logs)
+     shift
+     createFilesWithDate
      ;;   
     *)
       break
